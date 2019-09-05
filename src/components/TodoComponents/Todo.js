8f5d15d828820b1aa461ea4d44220799fc4fd442
AidenBow <AidenBow@users.noreply.github.com>
2019-09-04 21:25:32 -0700

    created basic components

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todo = (props) => {
+
+    return(
+        <div>
+            <p>props.</p>
+        </div>
+    )
+}
+
+export default Todo
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TodoForm = (props) => {
+
+    return(
+        <div>
+            <p>form</p>
+        </div>
+    )
+}
+
+export default TodoForm
